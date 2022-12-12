1418b75dda6ac205175ca367347d95c8a7f785b6
Devkaran Saw <Devkaran Saw@192.168.5.103>
2022-12-12 13:14:46 +0530

    p

---
+++ myproject1/src/myproject1/HelloWorld.java
@@ -0,0 +1,15 @@
+package myproject1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//System.out.println("Hello world");
+		
+		String[] fname= {"Ram","Shyam","Pawam"};
+		String[] Iname= {"Sharma","Pawar","Singh"};
+		for(int i=0;i<fname.length;i++) {
+			System.out.println("Name :"+ fname[i]+" "+Iname[i]);
+		}
+	}
+
+}
